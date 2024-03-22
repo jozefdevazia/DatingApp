4b46a05d6b2b6f60ea0250c1146ce8b30e41ec2b
Joseph Devassia <joseph.devassia@gmail.com>
2024-03-22 23:34:56 +0530

    End of section3

--- API/Program.cs
+++ API/Program.cs
@@ -9,11 +9,11 @@ builder.Services.AddDbContext<DataContext>(options=>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
