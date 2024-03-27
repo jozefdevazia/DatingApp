29c5bd3e461b4bf0e2f253f679d78fe36de98983
Joseph Devassia <joseph.devassia@gmail.com>
2024-03-27 18:00:08 +0530

    End of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,5 +1,8 @@
 
 
+using System.Security.Principal;
+using API.Extensions;
+
 namespace API.Entities
 {
     public class AppUser
@@ -8,6 +11,22 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth{get;set;}
+        public string KnownAs { get; set; }
+        public DateTime Created{get;set;} = DateTime.UtcNow;
+        public DateTime LastActive{get;set;} = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Intrduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos{get;set;} = new();
 
+       /* public int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        } */
     }
+   
 }
